eb39dc63e0d6aeeda923e1dea28e0f2239eace19
Nate Wong <nathaniel.h.wong@gmail.com>
2016-11-01 18:36:02 -0500

    Updated to properly create new movies and delete movie records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,12 +11,14 @@ class MoviesController < ApplicationController
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie = Movie.new
+
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
@@ -40,8 +42,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    movie = Movie.find(params[:id])
-
-    movie.destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
   end
 end
